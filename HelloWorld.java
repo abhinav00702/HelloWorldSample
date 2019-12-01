c799588ad9fefda728d43cc4522dc98a88dac7bb
Abhinav Borgohain <abhinav-mac@Abhinavs-MacBook-Pro-3.local>
2019-12-01 17:15:09 -0600

    initial commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,55 +1,11 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2509
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 CourierNewPSMT;\f1\fmodern\fcharset0 CourierNewPS-BoldMT;}
-{\colortbl;\red255\green255\blue255;\red15\green114\blue1;\red255\green255\blue255;\red0\green0\blue0;
-\red10\green82\blue135;\red0\green0\blue0;\red0\green0\blue255;}
-{\*\expandedcolortbl;;\cssrgb\c0\c50980\c0;\cssrgb\c100000\c100000\c100000;\cssrgb\c0\c0\c0\c84314;
-\cssrgb\c0\c40000\c60000;\cssrgb\c0\c0\c0;\cssrgb\c0\c0\c100000;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\parhyphenfactor20\partightenfactor0
-
-\f0\fs29\fsmilli14913 \cf2 \cb3 \expnd0\expndtw0\kerning0
 /* This is a simple Java program. 
-\fs30\fsmilli15064 \cf0 \
-\pard\pardeftab720\parhyphenfactor20\partightenfactor0
-
-\fs29\fsmilli14913 \cf0 \'a0\'a0\'a0\cf2 FileName : "HelloWorld.java". */
-\fs30\fsmilli15064 \cf0 \
-\pard\pardeftab720\parhyphenfactor20\partightenfactor0
-
-\f1\b\fs29\fsmilli14913 \cf5 class
-\f0\b0\fs30\fsmilli15064 \cf0  
-\fs29\fsmilli14913 HelloWorld 
-\fs30\fsmilli15064 \
-
-\fs29\fsmilli14913 \{ 
-\fs30\fsmilli15064 \
-\pard\pardeftab720\parhyphenfactor20\partightenfactor0
-
-\fs29\fsmilli14913 \cf0 \'a0\'a0\'a0\'a0\cf2 // Your program begins with a call to main(). 
-\fs30\fsmilli15064 \cf0 \
-
-\fs29\fsmilli14913 \'a0\'a0\'a0\'a0\cf2 // Prints "Hello, World" to the terminal window. 
-\fs30\fsmilli15064 \cf0 \
-
-\fs29\fsmilli14913 \'a0\'a0\'a0\'a0
-\f1\b \cf5 public
-\f0\b0\fs30\fsmilli15064 \cf0  
-\f1\b\fs29\fsmilli14913 \cf5 static
-\f0\b0\fs30\fsmilli15064 \cf0  
-\f1\b\fs29\fsmilli14913 \cf5 void
-\f0\b0\fs30\fsmilli15064 \cf0  
-\fs29\fsmilli14913 main(String args[]) 
-\fs30\fsmilli15064 \
-
-\fs29\fsmilli14913 \'a0\'a0\'a0\'a0\{ 
-\fs30\fsmilli15064 \
-
-\fs29\fsmilli14913 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0System.out.println(\cf7 "Hello, World"\cf0 ); 
-\fs30\fsmilli15064 \
-
-\fs29\fsmilli14913 \'a0\'a0\'a0\'a0\} 
-\fs30\fsmilli15064 \
-\pard\pardeftab720\parhyphenfactor20\partightenfactor0
-
-\fs29\fsmilli14913 \cf0 \} }
+   FileName : "HelloWorld.java". */
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+} 
